ea98b3abc702cd37235f04fdece75a4a83f01a92
Nepcam <cameron.nepe@gmail.com>
2020-08-04 19:58:44 +1200

    Add project files.

---
+++ ElectionVotes/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace ElectionVotes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.listBoxDisplay = new System.Windows.Forms.ListBox();
+            this.buttonCalculate = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // listBoxDisplay
+            // 
+            this.listBoxDisplay.FormattingEnabled = true;
+            this.listBoxDisplay.Location = new System.Drawing.Point(12, 135);
+            this.listBoxDisplay.Name = "listBoxDisplay";
+            this.listBoxDisplay.Size = new System.Drawing.Size(776, 303);
+            this.listBoxDisplay.TabIndex = 0;
+            // 
+            // buttonCalculate
+            // 
+            this.buttonCalculate.Location = new System.Drawing.Point(574, 28);
+            this.buttonCalculate.Name = "buttonCalculate";
+            this.buttonCalculate.Size = new System.Drawing.Size(214, 74);
+            this.buttonCalculate.TabIndex = 1;
+            this.buttonCalculate.Text = "Calculate";
+            this.buttonCalculate.UseVisualStyleBackColor = true;
+            this.buttonCalculate.Click += new System.EventHandler(this.buttonCalculate_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(267, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(277, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "How many votes are needed for a single seat in Parliment";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(267, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(237, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Click button to select file to calculate information ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonCalculate);
+            this.Controls.Add(this.listBoxDisplay);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ListBox listBoxDisplay;
+        private System.Windows.Forms.Button buttonCalculate;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ ElectionVotes/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace ElectionVotes
+{
+    public partial class Form1 : Form
+    {
+        //Name: Cameron Nepe
+        //ID  : 1262199
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        List<string> partyName = new List<string>();
+        List<int> numberVotes = new List<int>();
+
+        private void buttonCalculate_Click(object sender, EventArgs e)
+        {
+            //Declare variables
+            const string FILTER = "CSV Files|*.csv|ALL Files|*.*";
+            StreamReader reader;
+            string[] csvArray;
+            string line = "";
+            string party_name = "";
+            int number_votes = 0;
+            //int seatsInParliment = 0;
+            int totalSeatsPerParty = 0;
+
+            //SET the filter for the dialog control
+            openFileDialog1.Filter = FILTER;
+            //CHECK to see if the user has selected a file to open
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                //OPEN the selected file
+                reader = File.OpenText(openFileDialog1.FileName);
+                //REPEAT while it is not the end of the file
+                while (!reader.EndOfStream)
+                {
+                    try
+                    {
+                        //Read an entire line from the file
+                        line = reader.ReadLine();
+                        Console.WriteLine(line);
+                        //Split the line storing values in an array
+                        csvArray = line.Split(',');
+                        if (csvArray.Length == 2)
+                        {
+                            //Extract the values from the list into separate variables
+                            party_name = (csvArray[0]);
+                            number_votes = int.Parse(csvArray[1]);
+
+                            //Add the values to the lists
+                            partyName.Add(party_name);
+                            numberVotes.Add(number_votes);
+
+                            ////Display values into listBox
+                            listBoxDisplay.Items.Add(party_name.PadRight(20) + number_votes + totalSeatsPerParty);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Error: " + line);
+                        }
+                    }
+                    catch
+                    {
+                        MessageBox.Show("Error: " + line);
+                    }
+                }                
+            }
+            else
+            {
+                Console.WriteLine("This is the wrong file");
+            }
+
+            // Calculate how many seats a party would get based on the number of votes
+            // Get the total votes
+            int total_votes = numberVotes.Sum();
+            // Seats in Parliment
+            int parlimentSeats = 120;
+            // Divide the total number of votes by parliment seats
+            int votes_needed = total_votes / parlimentSeats;
+
+            Console.WriteLine(votes_needed);
+
+            for (int i = 0; i <= partyName.Count; i++)
+            {
+                //how many seats each party gained
+                totalSeatsPerParty = numberVotes.Sum() / votes_needed;
+                //seatsInParliment = numberVotes[i] / total_votes;
+                //Console.WriteLine(seatsInParliment);
+            }
+        }
+    }
+}
